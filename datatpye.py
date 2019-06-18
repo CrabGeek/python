d6a7ea829685fa20071beb3616377e0a06d1892a
lol9833 <lol9833@github.com>
2019-06-18 14:13:10 -0500

    The new version

--- datatpye.py
+++ datatpye.py
@@ -1,4 +1,4 @@
-
+import math
 #Numbers:
 a = 1
 b = 10
@@ -28,6 +28,46 @@ print(a // b) #整除
 print(a % b)
 print(a ** b) # a^b
 
+#举例3: 强制转化
 case = int(a)
 print (case)
 
+the_string = "10"
+case1 = int(the_string)
+print (case1)
+
+the_string2 = "1.0"
+case2 = float(the_string2)
+print(case2)
+
+case3 = 999
+the_string3 = str(case3)
+print(the_string3)
+
+case4 = 78
+the_string4 = chr(case4)
+print(the_string4)
+
+the_string5 = "a"
+case5 = ord(the_string5)
+print(case5)
+
+#练习：------------------------------------------------------------
+"""
+将华氏温度转换为摄氏温度
+F = 1.8C + 32
+"""
+f = float(input("please entry degree of temperature in f"))
+c = (f - 32) / 1.8
+print (c)
+
+"""
+输入半径计算圆的周长和面积
+
+"""
+r = float(input ("please the radiation of the cycle"))
+area = math.pi * (r ** 2)
+perimeter = 2 * math.pi * r
+
+print ("The area is %.2f" % area) #保留两位小数
+print ("The perimeter is %.2f" % perimeter)
