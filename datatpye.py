16ad3024c745fc4d9507adb75bd801e684d9d9e4
lol9833 <lol9833@github.com>
2019-06-18 13:45:46 -0500

    This is the version 2

--- datatpye.py
+++ datatpye.py
@@ -10,3 +10,24 @@ s = "Hello"
 
 print (s[1])
 print (s[4])
+
+#举例1:input输入以及格式化输出符号
+a = int(input ('a = '))
+b = int (input ('b = '))
+
+print ("%d + %d = %d" % ( a, b, a + b))
+
+#举例2:使用变量保存数据并进行算术运算
+a = 321
+b = 123
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b) #整除
+print(a % b)
+print(a ** b) # a^b
+
+case = int(a)
+print (case)
+
