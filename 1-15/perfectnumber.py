719f22c9412bd6905dc8028fa28107feac69d543
lol9833 <lol9833@github.com>
2019-06-20 00:02:14 -0500

    perfectnumber

---
+++ 1-15/perfectnumber.py
@@ -0,0 +1,14 @@
+import math
+
+num = 1
+
+
+
+while True:
+    accumlator = 0
+    for i in range(1, num):
+        if num % i == 0:
+            accumlator += i
+    if accumlator == num:
+        print (num)
+    num += 1
